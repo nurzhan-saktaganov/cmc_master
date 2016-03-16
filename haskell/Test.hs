e9d1da3de90d419ca43809254bb8f35a29b9c716
Nurzhan Saktaganov <nur92world@mail.ru>
2016-03-16 16:17:19 +0600

    Haskell Homework

--- haskell/Test.hs
+++
@@ -1,9 +0,0 @@
-{-# LANGUAGE FlexibleInstances #-}
-newtype Tup a b = Tup (a, b)
-
-instance Show (Tup Int Int) where
-  show (Tup tup) = show tup
-
-instance Show (Tup String Int) where
-  show (Tup (s, int)) = s ++ ": " ++ show int
-
