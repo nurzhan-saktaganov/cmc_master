b360666628e2a53cbdcc83beb1fad623c4093b1c
Nurzhan Saktaganov <nsaktaganov@yandex-team.ru>
2017-10-16 20:44:19 +0300

    delete c++ complex class

--- wavelet/testComplex.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include "Complex.hpp"
-
-int main(void){
-	Complex c0, c1(1), c2(1.2,2);
-	std::cout << c0 << " " << c1 << " " << c2 << std::endl;
-	std::cout << ((c0 + 1) / Complex(0,1)) << " " << c1 * Complex(0,1) << " " << c2 / 2 << std::endl;
-}
