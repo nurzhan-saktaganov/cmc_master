b68f828ce3c1d11e72167f23b721d4c023136058
Nurzhan Saktaganov <nur92world@mail.ru>
2017-10-23 03:40:14 +0300

    add check for length up to 24

--- parallel_prac/2017autumn/task1/schedule.cpp
+++ parallel_prac/2017autumn/task1/schedule.cpp
@@ -11,28 +11,26 @@ public:
     int a, b;
 };
 
+// build schedule
 void make_schedule(std::vector<comparator> &schedule, int n);
 void sort_schedule(std::vector<comparator>& schedule, int lo, int hi, int max_line);
 void oddeven_merge_schedule(std::vector<comparator>& schedule, int lo, int hi, int r, int max_line);
 
+// print report
 int count_parallel_steps(std::vector<comparator>& schedule, int n);
 void print_report(std::vector<comparator> &schedule, int n);
+
+// checking functions
+void check_scheduler();
+void check_schedule(std::vector<comparator>& schedule, int n);
+void init_array(int *array, int n, int bit_mask);
+bool is_sorted(int *array, int n);
+void perform_schedule(std::vector<comparator>& schedule, int *array);
 
 void show_usage()
 {
     std::cout<<"Usage: ./schedule n\n"
             <<"\tn - fragments number"<<std::endl;
-}
-
-void print_report(std::vector<comparator> &schedule, int n)
-{
-    std::cout<<n<<" 0 0"<<"\n";
-    for (comparator &c : schedule) {
-        std::cout<<c.a<<" "<<c.b<<"\n";
-    }
-
-    std::cout<<schedule.size()<<"\n"
-        <<count_parallel_steps(schedule, n)<<std::endl;
 }
 
 int main(int argc, char *argv[])
@@ -54,6 +52,9 @@ int main(int argc, char *argv[])
     std::vector<comparator> schedule;
     make_schedule(schedule, n);
     print_report(schedule, n);
+
+    int max_check_length = 24;
+    // check_scheduler(max_check_length);
 
     return 0;
 }
@@ -102,4 +103,66 @@ int count_parallel_steps(std::vector<comparator>& schedule, int n)
     }
 
     return *(std::max_element(linear_steps.begin(), linear_steps.end()));
+}
+
+void print_report(std::vector<comparator> &schedule, int n)
+{
+    std::cout<<n<<" 0 0"<<"\n";
+    for (comparator &c : schedule) {
+        std::cout<<c.a<<" "<<c.b<<"\n";
+    }
+
+    std::cout<<schedule.size()<<"\n"
+        <<count_parallel_steps(schedule, n)<<std::endl;
+}
+
+//
+void check_scheduler(int max_length)
+{
+    for (int n = 1; n <= max_length; ++n) {
+        std::cout<<"Check for size "<<n<<std::endl;
+        std::vector<comparator> schedule;
+        make_schedule(schedule, n);
+        check_schedule(schedule, n);
+    }
+}
+
+void check_schedule(std::vector<comparator>& schedule, int n)
+{
+    int up_to = 1 << n;
+    auto array = new int[n];
+
+    for (int bit_mask = 0; bit_mask < up_to; ++bit_mask){
+        init_array(array, n, bit_mask);
+        perform_schedule(schedule, array);
+        if (!is_sorted(array, n)) {
+            std::cerr<<"Got error for size "<<n<<std::endl;
+            exit(0);
+        }
+    }
+}
+
+void init_array(int *array, int n, int bit_mask)
+{
+    for (int i = 0; i < n; i++) {
+        array[i] = bit_mask & 1;
+        bit_mask >>= 1;
+    }
+}
+
+bool is_sorted(int *array, int n)
+{
+    for (int i = 0; i < n - 1; ++i){
+        if (array[i] > array[i + 1]) return false;
+    }
+    return true;
+}
+
+void perform_schedule(std::vector<comparator>& schedule, int *array)
+{
+    for (comparator &c : schedule) {
+        if (array[c.a] > array[c.b]) {
+            std::swap(array[c.a], array[c.b]);
+        }
+    }
 }
