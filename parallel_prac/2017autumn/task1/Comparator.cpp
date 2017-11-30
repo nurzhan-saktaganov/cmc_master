fad4707879ddc7d822a80a0f7f735653c11f7ad1
Nurzhan Saktaganov <nur92world@mail.ru>
2017-11-30 04:27:47 +0300

    new method and renew Makefile

--- parallel_prac/2017autumn/task1/Comparator.cpp
+++ parallel_prac/2017autumn/task1/Comparator.cpp
@@ -5,3 +5,8 @@ int Comparator::get_pair(const int x) const
     if (x != a && x != b) throw x;
     return x == b ? a : b;
 }
+
+bool Comparator::contains(const int x) const
+{
+    return x == a || x == b;
+}
--- parallel_prac/2017autumn/task1/Comparator.hpp
+++ parallel_prac/2017autumn/task1/Comparator.hpp
@@ -4,5 +4,6 @@ class Comparator {
 public:
     Comparator(const int a = -1, const int b = -1) : a(a), b(b){}
     int get_pair(const int x) const;
+    bool contains(const int x) const;
     int a, b;
 };
